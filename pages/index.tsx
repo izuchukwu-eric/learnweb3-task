eb0d9a3fdded3c3af7257f2a5622c8773452ffa3
Izuchukwu Onukwube <57435925+izuchukwu-eric@users.noreply.github.com>
2022-09-05 04:11:11 +0100

    new commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,27 @@
 import type { NextPage } from 'next'
 import { LockClosedIcon } from '@heroicons/react/20/solid'
+import Logo from "../assets/logo-blue.png";
+import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
     <div className="w-full max-w-md space-y-8">
       <div>
-        <img
+        <Image
           className="mx-auto h-12 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          src={Logo}
           alt="learnweb3logo"
         />
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
           Sign in to your account
         </h2>
-        {/* <p className="mt-2 text-center text-sm text-gray-600">
-          Or{' '}
-          <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-            start your 14-day free trial
-          </a>
-        </p> */}
       </div>
       
-        <div>
+        <div className=''>
           <button
             type="submit"
-            className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700"
           >
             <span className="absolute inset-y-0 left-0 flex items-center pl-3">
               <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
